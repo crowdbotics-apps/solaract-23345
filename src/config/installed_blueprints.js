a9f610a766a5dea84d23670659bd6769ff3bbbb5
Crowdbotics <team@crowdbotics.com>
2020-12-13 15:56:48 +0000

    add package blueprint-react-native-feed-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Feed5184801', human_name: 'Feed 5', access_route: 'Feed5184801'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
